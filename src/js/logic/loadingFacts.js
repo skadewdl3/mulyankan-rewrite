86dec4ca5d8b2a1b7659406e2992435dde7256a5
Soham K <sohamk10@gmail.com>
2021-04-24 21:38:13 +0530

    Removed Number Facts during loading screen. Instead added quotes.

--- src/js/logic/loadingFacts.js
+++ src/js/logic/loadingFacts.js
@@ -2,33 +2,10 @@ import randomArray from 'unique-random-array';
 import axios from 'axios';
 
 let arr = [
-  { type: 'category', content: 'trivia' },
-  { type: 'category', content: 'math' },
-  { type: 'category', content: 'year' },
-  { type: 'category', content: 'date' },
   {
-    type: 'fact',
-    content: 'Copy and Paste was invented in the 1960s by Larry Tesler.',
+    type: 'cat',
   },
-  {
-    type: 'fact',
-    content:
-      "An apple didn't actully fall on Newton's head, it fell in front of him.",
-  },
-  {
-    type: 'fact',
-    content:
-      'The best number is 73. Why? 73 is the 21st prime number. Its mirror, 37, is the 12th and its mirror, 21, is the product of multiplying 7 and 3 and in binary 73 is a palindrome, 1001001, which backwards is 1001001.',
-  },
-  {
-    type: 'fact',
-    content:
-      'The mathematician Ramanujan once proved that: 1 + 2 + 3 + ... upto infinity is equal to -1/12.',
-  },
-  {
-    type: 'url',
-    content: 'https://catfact.ninja/fact',
-  },
+  { type: 'quote', content: 'random' },
 ];
 
 let types = randomArray(arr);
@@ -36,12 +13,12 @@ let types = randomArray(arr);
 export const randomFact = async () => {
   let res = '';
   let obj = types();
-  if (obj.type === 'category') {
-    res = await axios.get(`http://numbersapi.com/random/${obj.content}`);
-  } else if (obj.type === 'url') {
-    res = await axios.get(obj.content);
-  } else if (obj.type === 'fact') {
-    res = obj.content;
+  if (obj.type === 'quote') {
+    let { data } = await axios.get(`https://api.quotable.io/random`);
+    console.log(data);
+    res = `${data.content} —${data.author}`;
+  } else if (obj.type === 'cat') {
+    res = await axios.get('https://catfact.ninja/fact');
   }
   return res;
 };
