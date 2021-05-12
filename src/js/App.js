bb00c847af76b9f5c4a15703df84dc5b86646541
Soham K <sohamk10@gmail.com>
2021-05-12 08:49:48 +0530

    Updated Redirects

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Landing from './layouts/Landing';
 import Editor from './layouts/Editor';
@@ -7,12 +7,12 @@ import Docs from './layouts/Docs';
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Route exact path="/" component={Landing} />
       <Route path="/editor" component={Editor} />
       {/* <Route path="/docs" component={Docs} /> */}
       <Route path="/docs/:id?" component={Docs} />
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
